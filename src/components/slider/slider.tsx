ad6a923b9a980e8f8cf67303f6098a5bf3abe250
Sunil Pandey <sunil.pandey@iongroup.com>
2019-02-13 10:42:14 +0530

    Added keyboard event of moving slowly

--- src/components/slider/slider.tsx
+++ src/components/slider/slider.tsx
@@ -5,6 +5,8 @@ import { Component, State, Element, Prop, EventEmitter, Event } from '@stencil/c
 })
 
 export class Slider {
+    private Left_Arrow = 37;
+    private Right_Arrow = 39;
     @Prop() maxValue: number = 255;
     @Prop() tickSize: number = 50;
     @State() left: string;
@@ -16,6 +18,7 @@ export class Slider {
 
     private mouseDown: boolean;
     private mainContainer: HTMLElement;
+    private draggerWidth: number;
     componentWillLoad() {
         this.left = "0px";
         document.addEventListener("mouseup", () => {
@@ -29,7 +32,11 @@ export class Slider {
 
     componentDidLoad() {
         this.mainContainer = this.sliderElement.querySelector('.main-container');
-
+        this.mainContainer.parentElement.addEventListener("keydown", (event: KeyboardEvent) => {
+            this.onKeyPress(event);
+        })
+        let dragger = this.sliderElement.querySelector('.dragger');
+        this.draggerWidth = parseInt(window.getComputedStyle(dragger).width, 10);
         var width = parseInt(window.getComputedStyle(this.mainContainer).width, 10);
         this.allTickPoints = [];
         var tickValue = this.maxValue/this.tickSize;
@@ -54,26 +61,39 @@ export class Slider {
     private changePosition($event) {
         if(this.mouseDown) {
             let value = ($event.clientX - this.getElementOffset(this.mainContainer))
-            if(value < 0) {
-                value = 0;
-            }
-            let width = parseInt(window.getComputedStyle(this.mainContainer).width, 10);
-            let dragger = this.sliderElement.querySelector('.dragger');
-            let draggerWidth = parseInt(window.getComputedStyle(dragger).width, 10);
-            if((value + draggerWidth) > width) {
-                value = width - draggerWidth;
-            }
-            this.onValueUpdated.emit({value: (this.maxValue/(width - draggerWidth)) * value})
-            this.left = value + "px";
+            this.setDraggerPosition(value);
         }
     }
 
+    private setDraggerPosition(value) {
+        if(value < 0) {
+            value = 0;
+        }
+        let width = parseInt(window.getComputedStyle(this.mainContainer).width, 10);
+        if((value + this.draggerWidth) > width) {
+            value = width - this.draggerWidth;
+        }
+        this.onValueUpdated.emit({value: (this.maxValue/(width - this.draggerWidth)) * value})
+        this.left = value + "px";
+    }
     private createTicks() {
         var ticks = [];
         for(let i = 0; i < this.allTickPoints.length; i++) {
             ticks.push(<div style={{ width: "1px", height: "100%", "background-color": "blue", position: "absolute", left: this.allTickPoints[i] + "px" }}></div>)
         }
         return ticks;
+    }
+
+    private onKeyPress = (event: KeyboardEvent) => {
+        switch(event.keyCode) {
+            case this.Left_Arrow:
+                this.setDraggerPosition(parseInt(this.left, 10) - 1);
+                break;
+            case this.Right_Arrow:
+                this.setDraggerPosition(parseInt(this.left, 10) + 1);
+                break;
+
+        }
     }
 
     render() {
